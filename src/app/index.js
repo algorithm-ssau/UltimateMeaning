66bd88cf3955b3a0039dc35e8e0c1c71da47e8ae
Knebergish <github@knebergish.fastmail.com>
2020-05-31 20:46:36 +0400

    Initial commit.

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch} from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+
+            </Switch>
+        </Router>
+    )
+}
+
+export default App
